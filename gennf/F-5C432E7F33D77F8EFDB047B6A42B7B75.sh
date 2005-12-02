0f1a72412a2bdf5c6ac67b3cbb6ccdcc3cff56b2
florenz <florenz>
2005-12-02 10:48:17 +0000

    Reinserted Meta-CVS code.
    When removing the code I forgot to purge. I have done this now.
    I didn't know how to re-activate the removed files. They were still in
    the MCVS-directory and I could issue mcvs restore but then I had
    lots of F-files in lost+found and did not know their original names.
    So I just added the files again. If someone knows a better solution to this
    problem, please tell me (or is there a feature missing?).

--- gennf/F-5C432E7F33D77F8EFDB047B6A42B7B75.sh
+++
@@ -1,7 +0,0 @@
-file_list="$file_list"' unix.o'
-mod_list="$mod_list"' unix'
-make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
-NEW_FILES="$file_list wrap.o"
-NEW_LIBS="$file_list wrap.o"
-NEW_MODULES="$mod_list"
-TO_LOAD='unix'
--- gennf/F-95E09617AEC5EDFE0EC797F917A64FA0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LATEXDOC=meta-cvs
-
-rm $LATEXDOC.aux $LATEXDOC.log $LATEXDOC.dvi $LATEXDOC.toc \
-  $LATEXDOC.idx $LATEXDOC.ind $LATEXDOC.ilg \
-  $LATEXDOC.idx.old $LATEXDOC.toc.old
