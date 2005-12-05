e1d43f4ef457425c425f1329855e41088f5e2ba3
florenz <florenz>
2005-12-05 22:42:30 +0000

    Corrected a bug ceoncering version number checking.

--- gennf/F-5D53AE4674717BF80056E24988D89A49.lisp
+++ gennf/F-5D53AE4674717BF80056E24988D89A49.lisp
@@ -38,7 +38,7 @@
 (defmacro def-c-call-out (sym &body args)
   `(def-call-out ,sym (:language :stdc) ,@args))
 
-#.(when (> (read-from-string (lisp-implementation-version)) 2.30)
+#.(when (string> (lisp-implementation-version) "2.30" :end1 4 :end2 4)
     (push :clisp-newer-than-2.30 *features*)
     (values))
 
