cc0528f448ab6e90fd1ce42ffa0d78fae7fb8c00
florenz <florenz>
2005-12-11 23:04:39 +0000

    The GPL header was missing.

--- gennf/F-DCFD91A2B9187774AFD3615DD030A174.lisp
+++ gennf/F-DCFD91A2B9187774AFD3615DD030A174.lisp
@@ -1,3 +1,24 @@
+;; Copyright 2005 Hannes Mehnert, Florian Lorenzen, Fabian Otto
+;;
+;; This file is part of gennf.
+;;
+;; gennf is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2 of the License, or
+;; (at your option) any later version.
+;;
+;; gennf is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with gennf; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+;;
+;; $Id: F-DCFD91A2B9187774AFD3615DD030A174.lisp,v 1.3 2005/12/11 23:04:39 florenz Exp $
+
+
 (defpackage :gennf-commit)
 
 (in-package :gennf-commit)
