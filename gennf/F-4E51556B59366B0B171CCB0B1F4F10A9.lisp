4a95a6346c26e3e31074cb54cdd4f637b2eb0292
florenz <florenz>
2006-01-24 19:56:46 +0000

    BOGUS.

--- gennf/F-4E51556B59366B0B171CCB0B1F4F10A9.lisp
+++ gennf/F-4E51556B59366B0B171CCB0B1F4F10A9.lisp
@@ -16,9 +16,9 @@
 ;; along with gennf; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ;;
-;; $Id: F-4E51556B59366B0B171CCB0B1F4F10A9.lisp,v 1.13 2006/01/24 19:52:41 florenz Exp $
+;; $Id: F-4E51556B59366B0B171CCB0B1F4F10A9.lisp,v 1.14 2006/01/24 19:56:46 florenz Exp $
 
-;; BOGUS234567
+;; BOGUS2345678
 
 (in-package :gennf)
 
