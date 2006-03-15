472fc9d82236a0251edfa51a199b9dfe697a846e
sigsegv <sigsegv>
2006-03-15 19:04:13 +0000

    mapping.lisp: - new macro defsubcommand - changed subcommands  to uses new macro

--- gennf/F-FC7FF8AB6284EA194323C1565C752386.lisp
+++ gennf/F-FC7FF8AB6284EA194323C1565C752386.lisp
@@ -16,7 +16,7 @@
 ;; along with gennf; if not, write to the Free Software
 ;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 ;;
-;; $Id: F-FC7FF8AB6284EA194323C1565C752386.lisp,v 1.29 2006/03/14 22:18:20 florenz Exp $
+;; $Id: F-FC7FF8AB6284EA194323C1565C752386.lisp,v 1.30 2006/03/15 19:04:13 sigsegv Exp $
 
 ;; Main module. Basic operations of gennf are implemented in this file.
 
@@ -48,17 +48,35 @@
     (format t "Command:~a~%Arguments:~a~%" command command-args)
     (unless command
       (error 'unknown-subcommand :text "Specify a subcommand!"))
-;;; (handler-case 			
     (dispatch-subcommand command command-args)
-	))
-;;;      (error () (error "Fehler!@!!!")))))
+    ))
+    ;(quit)))				
 
+;;  FIXME: Use global Variable with subcommands and lambdas.
 (defun dispatch-subcommand (command command-args)
-  (cond
-    ((equal command "add") (apply #'add command-args))
-    ((equal command "remove") (apply #'remove-file command-args))
- ))
-      
+  (apply (cdr(assoc command *subcommand-list* :test #'string=)) command-args))
+
+(defparameter *subcommand-list* nil)
+
+(defparameter *branch* nil)
+
+(defmacro defsubcommand (subcommand-name (&rest args) &body forms)
+  `(progn
+     ,(if (and (symbolp (first forms))
+	       (eq (first forms) :in-meta-directory))
+	  `(defun ,subcommand-name ,args
+	     (let ((*meta-directory* (find-meta-directory)))
+	       (in-meta-directory
+		 (let* ((*branch* (branch (read-sandbox-file)))
+			(*map-file* (merge-pathnames
+				     (branch-identifier-to-directory *branch*) *map-file*)))
+		   ,@(rest forms)))))
+	  `(defun ,subcommand-name ,args
+	     `(,@forms)))
+     (pushnew (cons (format nil "~(~a~)" ',subcommand-name)
+		     #',subcommand-name)
+	       *subcommand-list* :key #'car :test #'string=)))
+
 (defun initialize-repository (module root
 			      &key symbolic-name description)
   "Initializes a new repository at location root for the named module.
@@ -78,39 +96,59 @@ The identifier of the newly created branch is returned."
 	   origin-access origin-branch origin-change)
   identifier))
 
-(defun add (namestring)
+;; FIXME: multiple adds!
+(defsubcommand add (namestring)
+  :in-meta-directory
   ;; - find meta-dir
   ;; - create new mapping
   ;; - find mapfile
   ;; - add mapping to file 
-  (let ((*meta-directory* (find-meta-directory)))
-    (in-meta-directory
-      (let* ((branch (branch (read-sandbox-file)))
-	     (map-file (merge-pathnames
-			(branch-identifier-to-directory branch) *map-file*))
-	     (id (format nil "META/~a/~a" branch (guid-gen)))
-	     (path (merge-pathnames (pathname namestring)
-				    (port-path:get-parent-directory
-				     *meta-directory*)))
-	     (mapping (create-new-mapping :path path :id id)))
-	(add-mapping mapping map-file)))))
+  (let* ((id (format nil "META/~a/~a" *branch* (guid-gen)))
+	 (path (merge-pathnames (pathname namestring)
+				(port-path:get-parent-directory
+				 *meta-directory*)))
+	 (mapping (create-new-mapping :path path :id id)))
+    (add-mapping mapping *map-file*)))
 
-(defun remove-file (namestring)
-  (let ((*meta-directory* (find-meta-directory)))
-    (in-meta-directory
-      (let* ((branch (branch (read-sandbox-file)))
-	     (map-file (merge-pathnames
-			(branch-identifier-to-directory branch) *map-file*)))
-	(remove-mapping namestring map-file)))))
+(defsubcommand remove-file (namestring)
+  :in-meta-directory
+  (let ((mapping (get-mapping namestring *map-file*)))
+    (remove-mapping mapping *map-file*)
+    (delete-file (path mapping))))
 
-(defun move (namestring1 namestring2)
-    (let ((*meta-directory* (find-meta-directory)))
-    (in-meta-directory
-      (let* ((branch (branch (read-sandbox-file))))
-	(format t "NOT Yet implemented")
-	))))
+(defsubcommand move (namestring1 namestring2)
+  :in-meta-directory
+  (let* ((mapping1 (get-mapping namestring1 *map-file*))
+	 (path (merge-pathnames (pathname namestring2) 
+				(port-path:get-parent-directory *meta-directory*)))
+	 (new-mapping (create-new-mapping :kind :file 
+					  :path path
+					  :id (id mapping1))))
+    (remove-mapping mapping1 *map-file*)
+    (add-mapping new-mapping *map-file*)
+    (sync new-mapping)
+    (delete-file (path mapping1))))
 
-(defun main ()
-  "Like main in C."
-  (format t "This is gennf. It is not operable yet. Sorry.~%")
-  (quit))
+;;;; defsubcommand
+;;;; =============
+;;;; In configuration.lisp werden die globalen Variablen *xxx-file*
+;;;; als pathspecs definiert. Sie enthalten aber nur die
+;;;; Namenskomponente und sind keine absoluten Pfadangaben und können
+;;;; deswegen auch nicht direkt zum öffnen von Dateien verwendet
+;;;; werden.
+
+;;;; Fuer die einheitliche Erzeugung von subcommands wäre es von
+;;;; Vorteil, wenn es globale Variablen in Namesstyle von *xxx-file*
+;;;; gibt. Diese Variablen können dann von dem makro dynamisch
+;;;; gesetzt werden.  Mittels dieser File-Variablen wird ein einheitliches
+;;;; Interface geschaffen.
+
+;;;; Allerdings ist dafür Voraussetzung, daß die jetzigen Variablen
+;;;; in *xxx-file-name* umbenannt werden, um eine
+;;;; Namensunabhaengigkeit von GENNF zu erhalten. Diese neuerzeugten
+;;;; Variablen koennen dann von den globalen File-Variablen benutzt
+;;;; werden.
+
+
+;; (defsubcommand test-subcommand2  (arg1 arg2 arg3) :in-meta-directory
+;; 	       (format t "~a ~a ~a" arg1 arg2 arg3))
